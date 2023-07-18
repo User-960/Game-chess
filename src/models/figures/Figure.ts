ccafc42702713980151b41f0be7962039a268e9c
Ivan <ivanvolodin960@gmail.com>
2023-07-18 14:12:01 +0300

    feat: add logic for method canMove in base class Figure

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,38 +1,39 @@
-import { Cell } from '../Cell'
-import { Colors } from '../Colors'
 import logo from '/images/black-king.png'
 
+import { Cell } from '../Cell'
+import { Colors } from '../Colors'
+
 export enum FigureNames {
-  FIGURE = 'Figure',
-  KING = 'King',
-  KNIGHT = 'Knight',
-  PAWN = 'Pawn',
-  QUEEN = 'Queen',
-  ROOK = 'Rook',
-  BISHOP = 'Bishop'
+	FIGURE = 'Figure',
+	KING = 'King',
+	KNIGHT = 'Knight',
+	PAWN = 'Pawn',
+	QUEEN = 'Queen',
+	ROOK = 'Rook',
+	BISHOP = 'Bishop'
 }
 
 export class Figure {
-  color: Colors
-  logo: typeof logo | null
-  cell: Cell
-  name: FigureNames
-  id: number
+	color: Colors
+	logo: typeof logo | null
+	cell: Cell
+	name: FigureNames
+	id: number
 
-  constructor(color: Colors, cell: Cell) {
-    this.color = color
-    this.cell = cell
-    this.cell.figure = this
-    this.logo = null
-    this.name = FigureNames.FIGURE
-    this.id = Math.random()
-  }
+	constructor(color: Colors, cell: Cell) {
+		this.color = color
+		this.cell = cell
+		this.cell.figure = this
+		this.logo = null
+		this.name = FigureNames.FIGURE
+		this.id = Math.random()
+	}
 
-  canMove(target: Cell): boolean {
-    return true
-  }
+	canMove(target: Cell): boolean {
+		if (target.figure?.color == this.color) return false
+		if (target.figure?.name === FigureNames.KING) return false
+		return true
+	}
 
-  moveFigure(target: Cell) {
-    
-  }
+	moveFigure(target: Cell) {}
 }
