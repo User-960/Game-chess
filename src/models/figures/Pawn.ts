f3f623dc7f42790ed40b9e54bd79d1940e9fcb7b
Ivan <ivanvolodin960@gmail.com>
2023-07-18 17:57:08 +0300

    feat: add logic for move pawn

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -7,6 +7,8 @@ import { Colors } from '../Colors'
 import { Figure, FigureNames } from './Figure'
 
 export class Pawn extends Figure {
+	isFirstStep: boolean = true
+
 	constructor(color: Colors, cell: Cell) {
 		super(color, cell)
 		this.logo = color === Colors.BLACK ? blackLogo : whiteLogo
@@ -15,6 +17,23 @@ export class Pawn extends Figure {
 
 	canMove(target: Cell) {
 		if (!super.canMove(target)) return false
-		return true
+		const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1
+		const firstStepDirection = this.cell.figure?.color === Colors.BLACK ? 2 : -2
+
+		if (
+			(target.y === this.cell.y + direction ||
+				(this.isFirstStep && target.y === this.cell.y + firstStepDirection)) &&
+			target.x === this.cell.x &&
+			this.cell.board.getCell(target.x, target.y).isEmpty()
+		) {
+			return true
+		}
+
+		return false
+	}
+
+	moveFigure(target: Cell) {
+		super.moveFigure(target)
+		this.isFirstStep = false
 	}
 }
