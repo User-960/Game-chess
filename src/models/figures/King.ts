ffbd55335d8e1270309202dcdc914fd32c07782b
Ivan <ivanvolodin960@gmail.com>
2023-07-18 19:47:58 +0300

    feat: add logic for move king

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -15,6 +15,26 @@ export class King extends Figure {
 
 	canMove(target: Cell) {
 		if (!super.canMove(target)) return false
-		return true
+
+		if (
+			((target.y === this.cell.y + 1 || target.y === this.cell.y - 1) &&
+				(target.x === this.cell.x + 1 || target.x === this.cell.x - 1)) ||
+			((target.x === this.cell.x + 1 || target.x - 1 === this.cell.x - 1) &&
+				(target.y === this.cell.y + 1 || target.y - 1 === this.cell.y - 1)) ||
+			((target.x === this.cell.x - 1 || target.x + 1 === this.cell.x + 1) &&
+				(target.y === this.cell.y - 1 || target.y + 1 === this.cell.y + 1))
+		) {
+			return true
+		}
+
+		// if (
+		// 	target.y === this.cell.y + direction &&
+		// 	(target.x === this.cell.x + 1 || target.x === this.cell.x - 1) &&
+		// 	this.cell.isEnemy(target)
+		// ) {
+		// 	return true
+		// }
+
+		return false
 	}
 }
